46884127f54ca224d1438113c794058c351804b8
Sydney Spaulding <cozysydmedia@gmail.com>
2024-01-22 16:37:38 -0800

    created navbar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import * as React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    
+    <Navbar />
+
+  );
+}
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand">Navbar</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+      </div>
+    </nav>
   );
 }
 
