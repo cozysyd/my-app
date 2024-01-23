7bd9c9cdf05fbb790b2459648737427cf8a2317c
Sydney Spaulding <cozysydmedia@gmail.com>
2024-01-22 16:24:18 -0800

    fixed errors

--- src/App.js
+++ src/App.js
@@ -35,21 +35,22 @@ function App() {
 }
 
 function List() {
-  <ul>
-        {list.map(function(item) {
-          return (
-            <li key={item.objectID}>
+  return (
+    <ul>
+      {list.map(function(item) {
+        return (
+          <li key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
             </span>
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
-            </li>
-          )
+          </li>
+          );
         })}
     </ul>
-
+  );
 }
 
 export default App;
