74f2382e81015bc5e4e826f362aaa42ae2880e23
Sydney Spaulding <cozysydmedia@gmail.com>
2024-01-22 17:02:23 -0800

    added header

--- src/App.js
+++ src/App.js
@@ -40,13 +40,14 @@ const list = [
 function App() {
   return (
     <nav className={"navbar navbar-default"}>
-    
-    <div className={"container-fluid"}>
-
-    <Navbar />
-
-    </div>
-    
+      <div className={"container-fluid"}>
+        <div className={"navbar-header"}>
+          <a className={"navbar-brand"} href={"/"}>{header}</a>
+        </div>
+        
+        <Navbar />
+      
+      </div>
     </nav>
 
   );
